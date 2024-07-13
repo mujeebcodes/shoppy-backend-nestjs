ca99c2ab7f40e46d943f9289e3b7123bdfac93b2
mujeebcodes <mailmujeeb@icloud.com>
2024-07-13 19:00:46 +0100

    implemented sign up route using postgres and prisma

---
+++ src/users/users.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { CreateUserRequest } from './dto/create-user.request';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createUser(data: CreateUserRequest) {
+    try {
+      return await this.prismaService.user.create({
+        data: { ...data, password: await bcrypt.hash(data.password, 10) },
+        select: { email: true, id: true },
+      });
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new UnprocessableEntityException('Email already exists');
+      }
+      throw error;
+    }
+  }
+}
