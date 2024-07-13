0812a05c8ece1ab7b2686f9b6b5f0497e087521e
mujeebcodes <mailmujeeb@icloud.com>
2024-07-13 23:27:54 +0100

    authentication/authorization with jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
+import { User } from '@prisma/client';
+import { Response } from 'express';
+import ms from 'ms';
+import { ConfigService } from '@nestjs/config';
+import { TokenPayLoad } from './token-payload.interface';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(user: User, response: Response) {
+    const expires = new Date();
+    expires.setMilliseconds(
+      expires.getMilliseconds() +
+        ms(this.configService.getOrThrow<string>('JWT_EXPIRATION')),
+    );
+
+    const tokenPayLoad: TokenPayLoad = { userId: user.id };
+    const token = this.jwtService.sign(tokenPayLoad);
+    response.cookie('Authentication', token, {
+      secure: true,
+      httpOnly: true,
+      expires,
+    });
+
+    return { tokenPayLoad };
+  }
+
+  async verifyUser(email: string, password: string) {
+    try {
+      const user = await this.usersService.getUser({ email });
+      const authenticated = await bcrypt.compare(password, user.password);
+
+      if (!authenticated) {
+        throw new UnauthorizedException();
+      }
+      return user;
+    } catch (error) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { CreateUserRequest } from './dto/create-user.request';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { User } from '@prisma/client';
+import { Prisma, User } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
@@ -20,5 +20,9 @@ export class UsersService {
       }
       throw error;
     }
+  }
+
+  async getUser(filter: Prisma.UserWhereUniqueInput) {
+    return this.prismaService.user.findUniqueOrThrow({ where: filter });
   }
 }
