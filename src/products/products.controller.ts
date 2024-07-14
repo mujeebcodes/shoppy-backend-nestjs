58ed66567809f36c067354697521760b0f57a91f
mujeebcodes <mailmujeeb@icloud.com>
2024-07-15 00:35:28 +0100

    create product functionality

---
+++ src/products/products.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { CreateProductRequest } from './dto/create-product.request';
+import { CurrentUser } from '../auth/current-user.decorator';
+import { TokenPayLoad } from '../auth/token-payload.interface';
+import { ProductsService } from './products.service';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  async createProduct(
+    @Body() body: CreateProductRequest,
+    @CurrentUser() user: TokenPayLoad,
+  ) {
+    return this.productsService.createProduct(body, user.userId);
+  }
+}
