8bce6629efaff89b2374b2a833c50e82faae9073
mujeebcodes <mailmujeeb@icloud.com>
2024-07-19 17:57:29 +0100

    stripe webhooks to update items to sold

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -4,6 +4,7 @@ import { PrismaService } from '../prisma/prisma.service';
 import { promises as fs } from 'fs';
 import { join } from 'path';
 import { PRODUCT_IMAGES } from './product-images';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class ProductsService {
@@ -34,6 +35,10 @@ export class ProductsService {
     } catch (error) {
       throw new NotFoundException(`Product not found with ID ${productId}`);
     }
+  }
+
+  async update(productId: number, data: Prisma.ProductUpdateInput) {
+    await this.prismaService.product.update({ where: { id: productId }, data });
   }
 
   private async imageExists(productId: number) {
